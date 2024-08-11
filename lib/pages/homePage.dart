a605798f8e4fc9eca0e3a256ce71b8d47d548c0f
Faisal <138551448+FaisalNadaf@users.noreply.github.com>
2024-08-11 23:37:27 +0530

    basic app setup

---
+++ lib/pages/homePage.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _homePageState();
+  }
+}
+
+class _homePageState extends State<HomePage> {
+  _homePageState();
+
+  late double _width, _height;
+
+  @override
+  Widget build(BuildContext context) {
+    _height = MediaQuery.of(context).size.height;
+    _width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: _height * 1,
+        width: _width * 1,
+        decoration: const BoxDecoration(color: Colors.purple),
+      ),
+    );
+  }
+}
